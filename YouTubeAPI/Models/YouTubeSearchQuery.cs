b7bbbc68c048c8d1dacbe7fb803e414c3306e2db
Sameer KOLPEKWAR <sameer.kolpekwar@socgen.com>
2021-07-21 12:20:03 +0530

    added models for search and results from API

--- YouTubeAPI/Models/YouTubeSearchQuery.cs
+++ YouTubeAPI/Models/YouTubeSearchQuery.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace YouTubeAPI.Models
 {
+    
     public class YouTubeSearchQuery
     {
+        [JsonProperty("type")]
+
+        public string Type { get; set; }
+        [JsonProperty("keyword")] 
+        public string Keyword { get; set; }
+
+        [JsonProperty("publishedAfter")] 
+        public DateTime PublishedAfter { get; set; }
     }
 }
